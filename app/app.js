2acf694487b1addf90566f65535aee5be6e0710a
aminhp93 <minhpn.org.ec@gmail.com>
2016-10-02 21:09:04 -0700

    express.js basics

---
+++ app/app.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var app = express();
+var dataFile = require('./data/data.json');
+
+app.set('port', process.env.PORT || 3000)
+
+app.get('/', function(req, res){
+	var info = '';
+	dataFile.speakers.forEach(function(item){
+		info += `
+		<li>
+			<h2>${item.name}</h2>
+			<p>${item.summary}</p>
+		</li>
+		`;
+	})
+	res.send(`
+		<h1>Academy Meetups</h1>
+		${info}
+	`);
+});
+
+var server = app.listen(app.get('port'), function(){
+	console.log("listening on port " + app.get('port'));
+});
+
+
+// var http = require('http');
+// var myServer = http.createServer(function(request, response){
+// 	response.writeHead(200, {"Content-Type": "text/html"});
+
+// 	response.write("<h1>Hello</h1>");
+// 	response.end();
+// });
+
+// myServer.listen(3000);
+
+// console.log('go to localhost:3000 on your browser')
+
